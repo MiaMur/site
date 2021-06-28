9208d68d25a213897d1a763278dd32ace11f3aa0
MiaMur <48892735+MiaMur@users.noreply.github.com>
2021-06-28 18:57:14 -0400

    Add files via upload

--- SalonRespMenu.js
+++ SalonRespMenu.js
@@ -18,3 +18,87 @@ navToggle.addEventListener("click", function () {
   // }
   links.classList.toggle("show-links");
 });
+
+
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+/*var img = document.querySelectorAll(".myImg");
+var modalImg = document.getElementById("img01");
+var close = document.querySelector(".close");
+var modal = document.querySelector(".modal");
+
+
+img.addEventListener("click", function(){ 
+  modal.classList.add("open-modal");
+});
+close.addEventListener("click", function(){
+  modal.classList.remove("modal");
+})*/
+
+
+
+// create references to the modal...
+var modal = document.getElementById('my-modal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('img-gallery-item');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+   
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+/*
+var img1 = document.getElementById("my-img1");
+var img2 = document.getElementById("my-img2");
+var img3 = document.getElementById("my-img3");
+var img4 = document.getElementById("my-img4");
+var img = document.querySelectorAll(".myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img1.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img2.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img3.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img4.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}*/
