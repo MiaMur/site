266cbeae1655257e4756a33a9f852b9c47a650f5
MiaMur <48892735+MiaMur@users.noreply.github.com>
2021-06-28 22:22:10 -0400

    Add files via upload

--- SalonRespMenu.js
+++ SalonRespMenu.js
@@ -45,17 +45,18 @@ var modal = document.getElementById('my-modal');
 var images = document.getElementsByClassName('img-gallery-item');
 // the image in the modal
 var modalImg = document.getElementById("img01");
-// and the caption in the modal
+var control = document.getElementById("my-modal")
 
 
 // Go through all of the images with our custom class
 for (var i = 0; i < images.length; i++) {
+ 
   var img = images[i];
   // and attach our click listener for this image.
   img.onclick = function(evt) {
     modal.style.display = "block";
     modalImg.src = this.src;
-   
+     
   }
 }
 
@@ -63,6 +64,7 @@ var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() {
   modal.style.display = "none";
+  
 }
 
 
